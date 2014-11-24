e6ca033c0dde742973c05656d10ffc8879de6e7a
Adam Brykajlo <adam.brykajlo@gmail.com>
2014-11-23 18:12:30 -0800

    modified test.c in main directory

--- test.c
+++ test.c
@@ -8,10 +8,12 @@
 
 void callback(char *buf)
 {
-	printf("%s", buf);
-	fflush(stdout);
-	if (strncmp(buf, "exit", 4) == 0) {
-		beagleblue_exit();
+	int value;
+	halosuit_relay_value((int) *buf, &value);
+	if (value == 0) {
+		halosuit_relay_switch((int) *buf, HIGH);
+	} else if (value == 1) {
+		halosuit_relay_switch((int) *buf, LOW);
 	}
 }
 
@@ -20,10 +22,13 @@ int main()
 	char buf[1024] = { 0 };
 	beagleblue_init(&callback);
 	halosuit_init();
-	double temp0;
+	double temp0, temp1, temp2, temp3;
 	while (1) {
 		halosuit_temperature_value(HEAD, &temp0);
-		sprintf(buf, "{\"temperature\":{\"temp0\":%.5f,\"temp1\":0.12345}}\n", (double)temp0);
+		halosuit_temperature_value(ARMPITS, &temp1);
+		halosuit_temperature_value(CROTCH, &temp2);
+		halosuit_temperature_value(WATER, &temp3);
+		sprintf(buf, "{\"temperature\":{\"temp0\":%.2f,\"temp1\":%.2f,\"temp2\":%.2f,\"temp3\":%.2f}}", temp0, temp1, temp2, temp3);
 		beagleblue_android_send(buf);
 		memset(buf, 0, sizeof(buf));
 		sleep(1);
