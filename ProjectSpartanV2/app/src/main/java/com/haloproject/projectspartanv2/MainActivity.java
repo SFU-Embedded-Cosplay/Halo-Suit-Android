855458bdf6c60165626d70a1c3f83b94412d13f0
Adam <adam@adam.(none)>
2015-02-16 17:03:37 -0800

    updating v2

--- ProjectSpartanV2/app/src/main/java/com/haloproject/projectspartanv2/MainActivity.java
+++ ProjectSpartanV2/app/src/main/java/com/haloproject/projectspartanv2/MainActivity.java
@@ -1,36 +1,57 @@
 package com.haloproject.projectspartanv2;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
 
 import com.haloproject.projectspartanv2.R;
 
 
 public class MainActivity extends ActionBarActivity {
+    static private FragmentManager mFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new LoadFragment())
                     .commit();
         }
+        mFragmentManager = getSupportFragmentManager();
     }
 
     public void tempCool(View view) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new LoadFragment()).commit();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new CoolingFragment())
+                .addToBackStack("test").commit();
     }
 
+    public void settings(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new SettingsFragment())
+                .addToBackStack("test").commit();
+    }
+
+    public void lighting(View view) {
+        mFragmentManager.beginTransaction()
+                .replace(R.id.container, new LightingFragment())
+                .addToBackStack("test").commit();
+    }
+
+    public void radar(View view) {
+        mFragmentManager.beginTransaction()
+                .replace(R.id.container, new RadarFragment())
+                .addToBackStack("test").commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,19 +75,66 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
+    static public class LoadFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_load, container, false);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mFragmentManager.beginTransaction().replace(R.id.container, new MainFragment()).commit();
+                }
+            }, 1000);
+        }
+    }
+
+    static public class LightingFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+    static public class RadarFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+
+    static public class MainFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+    static public class CoolingFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_settings, container, false);
+        }
+    }
+
+    static public class SettingsFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_settings, container, false);
         }
     }
 }
