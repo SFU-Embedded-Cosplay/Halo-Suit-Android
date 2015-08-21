46fadf248b9d81d97da4b7d2b6660981db13cde4
Tyler Rubenuik <trubenui@sfu.ca>
2015-08-20 23:27:34 -0700

    Modify cooling buttons so that they update when the JSON message is received from the BeagleBone to address issue #10.

--- app/src/main/java/com/haloproject/projectspartanv2/Fragments/CoolingFragment.java
+++ app/src/main/java/com/haloproject/projectspartanv2/Fragments/CoolingFragment.java
@@ -1,10 +1,13 @@
 package com.haloproject.projectspartanv2.Fragments;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.haloproject.bluetooth.AndroidBlue;
@@ -20,6 +23,15 @@ import com.haloproject.projectspartanv2.view.TopBar;
 public class CoolingFragment extends Fragment {
     private TempWheel waterTemp;
     private TextView flowPump;
+
+    private RadioButton peltierAuto;
+    private RadioButton peltierOff;
+
+    private RadioButton headFansOn;
+    private RadioButton headFansOff;
+
+    private RadioButton waterPumpAuto;
+    private RadioButton waterPumpOff;
 
     private TopBar mTopBar;
     private AndroidBlue mAndroidBlue;
@@ -51,55 +63,81 @@ public class CoolingFragment extends Fragment {
 
 
         mTopBar.setMenuName("Cooling");
+
+
+        // load and store views from xml file
         View view = inflater.inflate(R.layout.fragment_cooling, container, false);
         waterTemp = (TempWheel) view.findViewById(R.id.waterTemp);
         flowPump = (TextView) view.findViewById(R.id.flowPump);
 
-        //set onclick listeners
-        view.findViewById(R.id.peltierauto).setOnClickListener(new View.OnClickListener() {
+        peltierAuto = (RadioButton) view.findViewById(R.id.peltierauto);
+        peltierOff = (RadioButton) view.findViewById(R.id.peltieroff);
+
+        headFansOn = (RadioButton) view.findViewById(R.id.headfanson);
+        headFansOff = (RadioButton) view.findViewById(R.id.headfansoff);
+
+        waterPumpAuto = (RadioButton) view.findViewById(R.id.waterpumpauto);
+        waterPumpOff = (RadioButton) view.findViewById(R.id.waterpumpoff);
+
+        // set onclick listeners
+        peltierAuto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.peltier.auto();
             }
         });
-        view.findViewById(R.id.peltieroff).setOnClickListener(new View.OnClickListener() {
+        peltierOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.peltier.off();
             }
         });
-        view.findViewById(R.id.headfanson).setOnClickListener(new View.OnClickListener() {
+
+        headFansOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.headFans.on();
             }
         });
-        view.findViewById(R.id.headfansoff).setOnClickListener(new View.OnClickListener() {
+        headFansOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.headFans.off();
             }
         });
-        view.findViewById(R.id.waterpumpauto).setOnClickListener(new View.OnClickListener() {
+
+        waterPumpAuto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.waterPump.auto();
             }
         });
-        view.findViewById(R.id.waterpumpoff).setOnClickListener(new View.OnClickListener() {
+        waterPumpOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDeviceHandlerCollection.waterPump.off();
             }
         });
+
         mAndroidBlue.setOnReceive(new Runnable() {
             @Override
             public void run() {
                 waterTemp.setTemp(mDeviceHandlerCollection.waterTemperature.getValue());
-                int flow = mDeviceHandlerCollection.flowRate.getValue();
-                String newFlow = String.format("%d", flow);
 
+
+                int flow = mDeviceHandlerCollection.flowRate.getValue();
+
+                String newFlow = String.format("%d", flow);
                 flowPump.setText(newFlow);
+
+                peltierAuto.setChecked(mDeviceHandlerCollection.peltier.isAuto());
+                peltierOff.setChecked(mDeviceHandlerCollection.peltier.isOff());
+
+                headFansOn.setChecked(mDeviceHandlerCollection.headFans.isOn());
+                headFansOff.setChecked(mDeviceHandlerCollection.headFans.isOff());
+
+                waterPumpAuto.setChecked(mDeviceHandlerCollection.waterPump.isAuto());
+                waterPumpOff.setChecked(mDeviceHandlerCollection.waterPump.isOff());
             }
         });
         return view;
